9e62cb784295fffc4daf6eefd26b0d775febead6
Sathya Laufer <sathya@laufers.net>
2018-06-10 17:01:24 +0200

    Small fix

--- src/MyCentral.cpp
+++ src/MyCentral.cpp
@@ -531,7 +531,7 @@ bool MyCentral::handlePairingRequest(std::string& interfaceId, PMyPacket packet)
 				physicalInterface->sendPacket(response);
 			}
 		}
-		else if(packet->getRorg() == 0xA5 && (packet->payload()->at(3) & 0x80)) //4BS teach-in, variant 3; LRN type bit needs to be set
+		else if(packet->getRorg() == 0xA5 && (payload.at(3) & 0x80)) //4BS teach-in, variant 3; LRN type bit needs to be set
 		{
 			if(payload.size() < 4) return false;
 			int32_t eep = ((int32_t)(uint8_t)payload.at(0) << 16) | (((int32_t)(uint8_t)payload.at(1) >> 2) << 8) | (((uint8_t)payload.at(1) & 3) << 5) | ((uint8_t)payload.at(2) >> 3);
