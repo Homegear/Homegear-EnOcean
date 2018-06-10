79faee290af915c1e2158ed1931b0cbda361d262
Sathya Laufer <sathya@laufers.net>
2018-06-10 14:55:10 +0200

    Don't require to specify interface in createDevice when only one interface exists

--- src/MyCentral.cpp
+++ src/MyCentral.cpp
@@ -1223,7 +1223,12 @@ PVariable MyCentral::createDevice(BaseLib::PRpcClientInfo clientInfo, int32_t de
 		std::string serial = getFreeSerialNumber(address);
 		if(peerExists(deviceType, address)) return Variable::createError(-5, "This peer is already paired to this central.");
 
-		if(GD::physicalInterfaces.find(interfaceId) == GD::physicalInterfaces.end()) return Variable::createError(-6, "Unknown physical interface.");
+		if(!interfaceId.empty() && GD::physicalInterfaces.find(interfaceId) == GD::physicalInterfaces.end()) return Variable::createError(-6, "Unknown physical interface.");
+        if(interfaceId.empty())
+        {
+            if(GD::physicalInterfaces.size() > 1) GD::physicalInterfaces.size() > 1) return Variable::createError(-7, "Please specify the ID of the physical interface (= communication module) to use.");
+            interfaceId = GD::physicalInterfaces.begin()->second->getID();
+        }
 
 		std::shared_ptr<MyPeer> peer = createPeer(deviceType, address, serial, false);
 		if(!peer || !peer->getRpcDevice()) return Variable::createError(-6, "Unknown device type.");
