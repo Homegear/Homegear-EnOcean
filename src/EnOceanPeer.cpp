434a47689f7a862cf9126197470d6f0035b781ad
Dr. Sathya Laufer <s.laufer@homegear.email>
2021-11-04 23:02:07 +0100

    Added backwards compatability for set security profile

--- src/EnOceanPeer.cpp
+++ src/EnOceanPeer.cpp
@@ -2225,7 +2225,7 @@ bool EnOceanPeer::remanSetSecurityProfile(bool outbound, uint8_t index, uint8_t 
 
     setBestInterface();
     auto physicalInterface = getPhysicalInterface();
-    auto setSecurityProfile = std::make_shared<SetSecurityProfile>(0, getRemanDestinationAddress(), _remanFeatures->kRecomVersion == 0x11, _remanFeatures->kSetSecurityProfileHasAddresses, outbound, index, slf, rlc, aesKey, destinationId, sourceId);
+    auto setSecurityProfile = std::make_shared<SetSecurityProfile>(0, getRemanDestinationAddress(), _remanFeatures->kRecomVersion == 0x11 || getFirmwareVersion() < 1100, _remanFeatures->kSetSecurityProfileHasAddresses, outbound, index, slf, rlc, aesKey, destinationId, sourceId);
     auto response = physicalInterface->sendAndReceivePacket(setSecurityProfile,
                                                             _address,
                                                             2,
@@ -2367,7 +2367,7 @@ bool EnOceanPeer::remanUpdateSecurityProfile() {
     auto setSecurityProfile =
         std::make_shared<SetSecurityProfile>(0,
                                              getRemanDestinationAddress(),
-                                             _remanFeatures->kRecomVersion == 0x11,
+                                             _remanFeatures->kRecomVersion == 0x11 || getFirmwareVersion() < 1100,
                                              _remanFeatures->kSetSecurityProfileHasAddresses,
                                              false,
                                              0,
@@ -2389,7 +2389,7 @@ bool EnOceanPeer::remanUpdateSecurityProfile() {
       setSecurityProfile =
           std::make_shared<SetSecurityProfile>(0,
                                                getRemanDestinationAddress(),
-                                               _remanFeatures->kRecomVersion == 0x11,
+                                               _remanFeatures->kRecomVersion == 0x11 || getFirmwareVersion() < 1100,
                                                _remanFeatures->kSetSecurityProfileHasAddresses,
                                                true,
                                                0,
